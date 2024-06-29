14da8e4574009b863c90c39fd2429eb3ce0eb388
Zhihao Sun <z5456123@ad.unsw.edu.au>
2024-06-29 21:57:30 +1000

    add test in test.cpp for move and copy constructor

--- src/filtered_string_view.test.cpp
+++ src/filtered_string_view.test.cpp
@@ -6,9 +6,13 @@ TEST_CASE("filter me if you can") {
 	// for (char c = std::numeric_limits<char>::min(); c != std::numeric_limits<char>::max(); c++) {
 	// 	std::cout << fsv::filtered_string_view::default_predicate(c);
 	// }
+	auto sv1 = fsv::filtered_string_view{"bulldog"};
+	const auto copy = sv1;
 
-	auto pred = [](const char& c) { return c == 'a'; };
-	auto sv = fsv::filtered_string_view{"cat", pred};
-	std::cout << sv.size() << std::endl;
+	assert(copy.data() == sv1.data()); // pointers compare equal.
+
+	const auto move = std::move(sv1);
+	assert(sv1.data() == nullptr); // true: sv1's guts were moved into `move`
+
 	// REQUIRE(false);
 }
