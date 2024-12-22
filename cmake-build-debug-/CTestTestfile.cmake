38907cad5320b8599f768985f09f83ddf8145f4d
adoleguizi <t3e7899@163.com>
2024-12-22 16:08:24 +0800

    Initial commit

---
+++ cmake-build-debug-/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/e/6771_T2/ass2
+# Build directory: /mnt/e/6771_T2/ass2/cmake-build-debug-
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(filtered_string_view_test "filtered_string_view_test")
+set_tests_properties(filtered_string_view_test PROPERTIES  _BACKTRACE_TRIPLES "/mnt/e/6771_T2/ass2/CMakeLists.txt;70;add_test;/mnt/e/6771_T2/ass2/CMakeLists.txt;0;")
